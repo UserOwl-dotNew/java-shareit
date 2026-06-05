cfb1970cc8f292915f72546bb379ba76538a5362
owl <dmitrikojevnikov@yandex.ru>
2026-06-05 20:35:04 +0300

    Добавить работу с ошибками

--- src/main/java/ru/practicum/shareit/exception/ExceptionHandlers.java
+++ src/main/java/ru/practicum/shareit/exception/ExceptionHandlers.java
@@ -3,10 +3,13 @@ package ru.practicum.shareit.exception;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @ControllerAdvice("ru.practicum.shareit")
 @RestControllerAdvice
@@ -16,13 +19,6 @@ public class ExceptionHandlers {
     @ExceptionHandler({NotFoundException.class})
     public ErrorResponse handlerNotFoundException(final NotFoundException e) {
         log.error("Not found error: {}", e.getMessage(), e);
-        return new ErrorResponse(e.getMessage());
-    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler({ErrorIsNotOwner.class})
-    public ErrorResponse handlerNotOwnerException(final ErrorIsNotOwner e) {
-        log.error("Пользователь не является владельцем вещи: {}", e.getMessage(), e);
         return new ErrorResponse(e.getMessage());
     }
 
@@ -47,10 +43,27 @@ public class ExceptionHandlers {
         return new ErrorResponse(e.getMessage());
     }
 
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ExceptionHandler({ErrorIsNotOwner.class, ErrorIsNotBooker.class, ErrorIsNotOwnerAndBooker.class})
+    public ErrorResponse handleNotBookerOrOwnerException(final Exception e) {
+        log.error("Is not Owner or Booker: {}", e.getMessage(), e);
+        return new ErrorResponse(e.getMessage());
+    }
+
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler
     public ErrorResponse handlerThrowable(final Throwable e) {
         log.error("Unexpected error: ", e);
         return new ErrorResponse("Произошла непредвиденная ошибка." + e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorResponse handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        String errorMessage = e.getBindingResult().getAllErrors().stream()
+                .map(error -> error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+        log.error("Validation error: {}", errorMessage, e);
+        return new ErrorResponse("Ошибка валидации: " + errorMessage);
     }
 }
---
+++ src/main/java/ru/practicum/shareit/exception/UncorrectedDateException.java
@@ -0,0 +1,7 @@
+package ru.practicum.shareit.exception;
+
+public class UncorrectedDateException extends RuntimeException {
+    public UncorrectedDateException(String message) {
+        super(message);
+    }
+}
