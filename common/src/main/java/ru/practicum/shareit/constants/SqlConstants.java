92d7b0b1a457bd51e6bdbe5c4767fdaca3c378d5
owl <dmitrikojevnikov@yandex.ru>
2026-06-26 00:32:41 +0300

    Удалить не используемый импорт

--- common/src/main/java/ru/practicum/shareit/constants/SqlConstants.java
+++ common/src/main/java/ru/practicum/shareit/constants/SqlConstants.java
@@ -1,11 +1,8 @@
 package ru.practicum.shareit.constants;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 public class SqlConstants {
     public static final String CURRENT_TIMESTAMP = "CURRENT_TIMESTAMP";
     public static final String REQUEST_HEADER_SHARER_USER_ID = "X-Sharer-User-Id";
